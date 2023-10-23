9c5186151a0065d42236087f8c6cd4993c3fd3db
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-10-23 19:43:02 +0000

    nueva entrega

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
 };
+
+const suits = ["♦", "♥", "♠", "♣"];
+const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 10, "J", "Q", "K"];
+
+function randomCard() {
+  let card = cards[Math.floor(Math.random() * cards.length)];
+
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+
+  if (suit == "♦" || suit == "♥") {
+    document.getElementById("randomSuit").style.color = "red";
+    document.getElementById("randomCard").style.color = "red";
+    document.getElementById("randomSuit2").style.color = "red";
+  }
+
+  document.getElementById("randomCard").innerHTML = card;
+  document.getElementById("randomSuit").innerHTML = suit;
+  document.getElementById("randomSuit2").innerHTML = suit;
+}
